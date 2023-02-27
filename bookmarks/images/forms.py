18cae516be350839eea0d857195a01c10fdac282
Alex-Wo <blackdevops13@gmail.com>
2023-02-27 16:44:42 +0300

    Переопределён метод save() модели формы

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,3 +1,6 @@
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
 from .models import Image
 
@@ -19,3 +22,15 @@ def clean_url(self):
     if extension not in valid_extensions:
         raise forms.ValidationError('The given URL does not ' 'match valid image extensions.')
     return url
+
+
+def save(self, force_insert=False, force_update=False, commit=True):
+    image = super(ImageCreateForm, self).save(commit=False)
+    image_url = self.cleaned_data['url']
+    image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+    # Скачиваем изображение по указанному адресу
+    response = request.urlopen(image_url)
+    image.image.save(image_name, ContentFile(response.read()), save=False)
+    if commit:
+        image.save()
+    return image
