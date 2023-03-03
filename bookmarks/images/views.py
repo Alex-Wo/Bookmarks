58f72aef05fe8e370b1bc6a4d7e1a949a4ba47fa
Alex-Wo <blackdevops13@gmail.com>
2023-03-03 16:45:24 +0300

    Реализован постраничный вывод с помощью AJAX

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,8 @@ from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 # from common.decorators import ajax_required
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def image_detail(request, id, slug):
@@ -55,3 +57,25 @@ def image_like(request):
         finally:
             pass
     return JsonResponse({'status': 'ok'})
+
+
+@login_required
+def image_list(request):
+    """ Обработчик, формирующий QuerySet для получения всех изображений, сохранённых в закладки """
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если переданная страница не является числом, возвращаем первую
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # Если получили AJAX-запрос с номером страницы большим, чем их количество, возвращаем пустую страницу
+            return HttpResponse('')
+        # Если номер страницы больше, чем их количество, тогда возвращаем последнюю
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list.ajax.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
