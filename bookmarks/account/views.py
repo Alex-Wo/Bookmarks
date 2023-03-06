5273477d1078edaaf07ce10a067778a4767acdb9
Alex-Wo <blackdevops13@gmail.com>
2023-03-06 12:16:21 +0300

    Добавлены активности в обработчик

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -11,6 +11,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 # from common.decorators import ajax_required
 from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -39,6 +40,13 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # По умолчанию отображаем все действия
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # Если текущий юзер подписался на кого-то, отображаем действия только этих юзеров
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
